6dcc0678676abfc99e84139e55b3a283c620f7c2
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-02-25 03:22:34 +0100

    - Use datatable with angularjs

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,33 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular
+  .module('myApp.view1', ['ngRoute', 'datatables', 'ngResource'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
 
-.controller('View1Ctrl', [function() {
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
 
-}]);
+  .service ('appService', function($http) {
+    this.getPersons = function() {
+      return $http({
+        "method": "get",
+        "url": './view1/data.json'
+        });
+    };
+  })
+
+  .controller('View1Ctrl',['$scope', '$resource', '$location', '$http', 'appService', View1Ctrl]);
+
+  function View1Ctrl($scope, $resource, $location, $http, appService) {
+
+    appService.getPersons().then(function(persons){
+      $scope.persons = persons.data;
+    });
+
+
+
+  }
